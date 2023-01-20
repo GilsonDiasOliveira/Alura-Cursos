95af118714217f0396c15a01dc93302e37f8cc9a
GilsonDiasOliveira <gilson.gddo.pti@gmail.comm>
2023-01-20 00:15:49 -0300

    Upload Validação-Password

--- cypress/e2e/3-alura-pic/alurapic.cy.js
+++ cypress/e2e/3-alura-pic/alurapic.cy.js
@@ -14,4 +14,25 @@ describe ('Validação de campos obrigatórios na tela de login e registro', () 
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
     })
 
+    it('Verifica mensagens de email inválido', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click ();
+        // Inserindo nome Jacqueline no campo
+        cy.get('input[formcontrolname="email"]').type('Jacqueline')
+        //Validação do Email inválido
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+
+    })
+
+    it('Verifica mensagens de senha com menos de 8 caracteres', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click ();
+        // Inserindo senha 123
+        cy.get('input[formcontrolname="password"]').type('123')
+        cy.contains('button', 'Register').click ();
+        //Validação de senha com menos de 8 caracteres
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+
+    })
+
 })
